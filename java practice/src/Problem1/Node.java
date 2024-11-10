1be5aec110f93d3990dd905c858ed12d5a853258
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2024-11-10 18:04:27 +0530

    added space

--- java practice/src/Problem1/Node.java
+++ java practice/src/Problem1/Node.java
@@ -26,5 +26,5 @@ Node should have two properties - left and right of type Node.
 Node should also have an integer property - data.
 Create a class TreeSizeCalculator to calculate the size of a BinaryTree using multiple threads.
 TreeSizeCalculator constructor will be passed the root of the tree and ExecutorService to make new threads.
-TreeSizeCalculator should implement Callable<Integer> interface
+TreeSizeCalculator should implement Callable<Integer> interface.
 */
